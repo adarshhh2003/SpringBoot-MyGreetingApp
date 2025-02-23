16859cbaa131f578021b47fd4e96511a4e60a0d2
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-23 22:48:20 +0530

    [Adarsh] Update. message saving functionality added in GreetingService

--- MyGreetingApp/src/main/java/com/example/demo/service/GreetingService.java
+++ MyGreetingApp/src/main/java/com/example/demo/service/GreetingService.java
@@ -1,18 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Greeting;
+import com.example.demo.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GreetingService {
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public Greeting saveGreeting(String message) {
+        Greeting greeting = new Greeting(message);
+        return greetingRepository.save(greeting);
+    }
+
     public String getGreetingMessage(String firstName, String lastName) {
+        String message;
         if(firstName != null && lastName != null) {
-            return "Hello " + firstName + " " + lastName + " !";
+            message = "Hello " + firstName + " " + lastName + " !";
         } else if(firstName != null) {
-            return "Hello " + firstName + " !";
+            message =  "Hello " + firstName + " !";
         } else if(lastName != null) {
-            return "Hello " + lastName + " !";
+            message = "Hello " + lastName + " !";
         } else {
-            return "Hello World!";
+            message = "Hello World!";
         }
+        saveGreeting(message);
+        return message;
+    }
+
+    public Greeting getGreetingById(Long id) {
+        Optional<Greeting> greeting = greetingRepository.findById(id);
+        return greeting.orElse(null);
     }
 }
