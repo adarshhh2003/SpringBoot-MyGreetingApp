562dfe91bf5172d5a40d8c59cf67a1d3a242af12
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-23 23:38:52 +0530

    [Adarsh] Update. list all greeting message from database feature added

--- MyGreetingApp/src/main/java/com/example/demo/controller/GreetingController.java
+++ MyGreetingApp/src/main/java/com/example/demo/controller/GreetingController.java
@@ -6,6 +6,8 @@ import com.example.demo.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
@@ -50,5 +52,10 @@ public class GreetingController {
     @GetMapping("/{id}")
     public Greeting getGreeting(@PathVariable Long id) {
         return greetingService.getGreetingById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Greeting> getAllGreetings() {
+        return greetingService.getAllGreetings();
     }
 }
